a419365696b8f2440ba2b2c973f58095b5974b8e
John Doe <cuonga2242002@gmail.com>
2024-11-24 07:48:34 +0700

    admin-user

--- src/page/AdminUser.jsx
+++ src/page/AdminUser.jsx
@@ -1,5 +1,20 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import UserTable from "../components/admin/user/UserTable";
+import { useAdmin } from "../context/AdminContext";
 export default function AdminUser() {
-  return <div>AdminUser</div>;
+  const { users } = useAdmin();
+  console.log(users);
+
+  return (
+    <main className="admin-content">
+      <>
+        <p className="admin-content-title">LIST USER</p>
+        <Link to="create">
+          <button className="add-user-button">Add User</button>
+        </Link>
+        <UserTable users={users} />
+      </>
+    </main>
+  );
 }
