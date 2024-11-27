d4245629ab7da745af0a136e90d7961a5d650606
Trung Dat <nguyentrungdat6996@gmail.com>
2024-11-28 01:57:01 +0700

    Homepage Pagination + PaginationItem UI

---
+++ src/components/homepage/Pagination.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import PaginationItem from "./PaginationItem";
+export default function Pagination({ totalPages, currentPage }) {
+
+  return (
+    <ul className="pagination home-product__pagination">
+      <PaginationItem
+        isIcon={true}
+        icon={faAngleLeft}
+        link={`/page/${Math.max(currentPage - 1, 1)}`}
+      />
+      {pages.map((page) => (
+        <PaginationItem
+          key={page}
+          page={page}
+          isActive={page === currentPage}
+          link={`/page/${page}`}
+        />
+      ))}
+      <PaginationItem
+        isIcon={true}
+        icon={faAngleRight}
+        link={`/page/${Math.min(currentPage + 1, totalPages)}`}
+      />
+    </ul>
+  );
+}
